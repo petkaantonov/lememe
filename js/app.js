fc2437030ab58cfbe8ae3d3d32bd9043b3a9b4ce
rlemon <rob.lemon@gmail.com>
2012-05-08 16:10:22 -0400

    added timer to Konami to restart 10 seconds after no keyboard input

--- js/app.js
+++ js/app.js
@@ -133,7 +133,8 @@ if( $.cookie('lememe-api-key') ) {
 }
 
 var k_pattern = [38,38,40,40,37,39,37,39,66,65],
-    k_hold = k_pattern.splice(0);
+    k_hold = k_pattern.splice(0),
+    k_reset;
 $(window).on('keydown', function(e) {
 	if( k_hold.shift() === e.which && k_hold.length === 0) {
 		top_input.val(" Neva gonna give you up! ");
@@ -145,6 +146,10 @@ $(window).on('keydown', function(e) {
 		font_size.val(28);
 		k_hold = k_pattern.splice(0);
 	}
+	clearTimeout(k_reset);
+	k_reset = setTimeout(function() {
+		k_hold = k_pattern.splice(0);
+	}, 1000 * 10);
 });
 
 draw();
